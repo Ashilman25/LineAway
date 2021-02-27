7082b1c46e5906983640385c0673e90d8b4f4707
Amortal25 <Andrew.Shilman@gmail.com>
2021-02-26 18:58:54 -0800

    Initial Commit

---
+++ LineAwaySwiftUI/LineAwaySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineAwaySwiftUIApp.swift
+//  LineAwaySwiftUI
+//
+//  Created by Andrew Shilman on 2/26/21.
+//
+
+import SwiftUI
+
+@main
+struct LineAwaySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
